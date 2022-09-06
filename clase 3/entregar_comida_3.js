6345b5e4bc33a2f71f8c8c91e9354eaa96ebac1d
Gaspar De Carlo <gaspar.decarlo@gmail.com>
2022-09-05 21:26:37 -0300

    se agrega ejemplo con promise.all

---
+++ clase 3/entregar_comida_3.js
@@ -0,0 +1,40 @@
+async function entregarComida() {
+  console.log("Empieza el proceso..");
+  const ensaladaPromise = getEnsalada();
+  const hamburguesaPromise = getHamburguesa();
+  const bebidaPromise = getBebida();
+
+  const [ensalada, hamburguesa, bebida] = await Promise.all([
+    ensaladaPromise,
+    hamburguesaPromise,
+    bebidaPromise,
+  ]);
+  console.log(`Su pedido de ${ensalada} ${hamburguesa} ${bebida} esta listo`);
+}
+
+function getEnsalada() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("🥗");
+    }, 2000);
+  });
+}
+
+function getHamburguesa() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("🍔");
+    }, 3000);
+  });
+}
+
+//no tenes control
+function getBebida() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("🍺");
+    }, 4000);
+  });
+}
+
+entregarComida();
